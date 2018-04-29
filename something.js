1d658c9e02d08bb46783e46a6ec08aca6fc2edea
ZBNsoft <32881705+ZBNsoft@users.noreply.github.com>
2018-04-29 08:56:11 +0300

    First Commit
    New site.

---
+++ something.js
@@ -0,0 +1,5 @@
+jQuery('Document').ready(function(){
+  $('.Download').on('click', function() {
+    window.location = $('https://doc-0o-2c-docs.googleusercontent.com/docs/securesc/14etgtjrmgj90rijejheemv2jg3548dm/2ruiv5uvjtaacm0ct63paql0l1er4kt8/1524924000000/01884634207652238043/01884634207652238043/1guyZ7imzfev4OdIyIkE9VG0U9zYFWwjF?e=download&nonce=koreeepsgnhrc&user=01884634207652238043&hash=umnb5jh6b5kqq969h1l0crqairtk0fco').val();
+});
+});
